5dff1254d5cac53e56b7861c98c194dfb6d9bcd7
Neha Penkalkar <nehapenkalkar@gmail.com>
2021-04-13 15:19:09 +0530

    source tree

--- PlayerApi/PlayerVC.swift
+++ PlayerApi/PlayerVC.swift
@@ -56,7 +56,6 @@ extension PlayerVC: UITableViewDataSource, UITableViewDelegate{
         let arr = countryNameDict.value(forKey: "India") as! [NSDictionary]
         let demo = arr[indexPath.row]
         cell.textLabel?.text = demo.value(forKey: "name") as? String ?? ""
-        print("Source Tree testing")
         return cell
     }
     
