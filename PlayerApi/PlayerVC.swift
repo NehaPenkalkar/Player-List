76fa312ea34daf41b06bca3163e8cfc10a4646f1
Neha Penkalkar <nehapenkalkar@gmail.com>
2021-04-13 21:07:05 +0530

    second vc sort

--- PlayerApi/PlayerVC.swift
+++ PlayerApi/PlayerVC.swift
@@ -10,7 +10,7 @@ import UIKit
 class PlayerVC: UIViewController{
     var countryNameDict = NSDictionary()
     var countryName = ""
-    var playerNameArr = [String]()
+    var playerNameArr = [NSDictionary]()
     
     @IBOutlet weak var playerTV: UITableView!
     override func viewDidLoad() {
@@ -20,6 +20,8 @@ class PlayerVC: UIViewController{
         // Do any additional setup after loading the view.
         apiCall()
     }
+    
+    
     func apiCall(){
         URLSession.shared.dataTask(with: URL(string: "http://test.oye.direct/players.json")!) { (data, resp, err) in
             
@@ -32,6 +34,9 @@ class PlayerVC: UIViewController{
                 do{
                     let jsonResp = try JSONSerialization.jsonObject(with: dataResp, options: .mutableLeaves) as! NSDictionary
                     self.countryNameDict = jsonResp
+                    self.playerNameArr = self.countryNameDict.value(forKey: "\(self.countryName)") as! [NSDictionary]
+                    
+                    print(self.playerNameArr)
                     
                     DispatchQueue.main.async {
                         self.playerTV.reloadData()
@@ -43,6 +48,21 @@ class PlayerVC: UIViewController{
             }
         }.resume()
     }
+
+    
+    @IBAction func sortNameBtn(_ sender: Any) {
+         
+        let sorted = playerNameArr.sort { left, right -> Bool in
+            guard let rightKey = right["name"] as? String else { return true }
+            guard let leftKey = left["name"] as? String else { return false }
+            return leftKey < rightKey
+        }
+            
+        
+        DispatchQueue.main.async {
+            self.playerTV.reloadData()
+        }
+    }
 }
 
 extension PlayerVC: UITableViewDataSource, UITableViewDelegate{
@@ -52,9 +72,10 @@ extension PlayerVC: UITableViewDataSource, UITableViewDelegate{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerTVC") as! PlayerTVC
-        let arr = countryNameDict.value(forKey: "\(countryName)") as! [NSDictionary]
-        let arr1 = arr[indexPath.row]
+        //let arr = countryNameDict.value(forKey: "\(countryName)") as! [NSDictionary]
+        let arr1 = playerNameArr[indexPath.row]
         cell.PlayerLbl.text = arr1.value(forKey: "name") as? String ?? ""
         
         let cap = arr1.value(forKey: "captain")
@@ -74,12 +95,6 @@ class PlayerTVC: UITableViewCell{
     
     @IBOutlet weak var PlayerLbl: UILabel!
     
-    @IBAction func sortLastBtn(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func sortNameBtn(_ sender: Any) {
-        
-    }
+
     
 }
